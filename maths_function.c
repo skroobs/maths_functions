455a2bd78aa52498c237d57bfa41d6dd39da50a6
Thomas Hogan <P14144468@GWHXX584LIN011W.lec-admin.dmu.ac.uk>
2016-01-28 15:59:31 +0000

    rem funct added

--- maths_function.c
+++ maths_function.c
@@ -20,18 +20,24 @@ int div (int x, int y)
     return x/y;
 }
 
+int rem (int x, int y)
+{
+    return x%y;
+}
+
 
 int main()
 {
 	int a = 5;
 	int b = 7;
-	int i = 0;
+	int c = 42;
 
 
 	printf("add(%i, %i) = %3i\n",a, b, add(a, b));
 	printf("sub(%i, %i) = %3i\n",a, b, sub(a, b));
 	printf("mult(%i, %i) = %3i\n",a, b, mult(a, b));
 	printf("div(%i, %i) = %3i\n",a, b, div(a, b));
+	printf("rem(%i, %i) = %3i\n", c, a, rem(c, a));
 
 	
 
