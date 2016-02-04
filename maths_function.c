7f4912cf249c5162c9a190e1192f327b69b4eef8
Thomas Hogan <P14144468@GWHXX584LIN015W.lec-admin.dmu.ac.uk>
2016-02-04 15:51:47 +0000

    added smallest and biggest number functionality

--- maths_function.c
+++ maths_function.c
@@ -31,6 +31,8 @@ int main()
 	int a = 5;
 	int b = 7;
 	int c = 42;
+	int numbers[4];
+	int biggest, smallest, n = 0;
 
 
 	printf("add(%i, %i) = %3i\n",a, b, add(a, b));
@@ -39,7 +41,29 @@ int main()
 	printf("div(%i, %i) = %3i\n",a, b, div(a, b));
 	printf("rem(%i, %i) = %3i\n", c, a, rem(c, a));
 
-	
+	printf("Enter 5 numbers: \n");
+	for (n=0; n<5; n++){
+		scanf("%i", &numbers[n]);
+	}
+
+
+	biggest = numbers[0];
+	for (n=0; n<5; n++){
+		if (numbers[n+1] > biggest){
+			biggest = numbers[n+1];
+		}
+	}
+
+	printf("Biggest number of the 5 you typed in: %i\n", biggest);
+
+	smallest = numbers[0];
+
+	for (n=0; n<4; n++){
+		if (numbers[n+1] < smallest){
+			smallest = numbers[n+1];
+		}
+	}
+	printf("Smallest number of the 5 you typed in: %i\n", smallest);
 
 	return 0;
 }
